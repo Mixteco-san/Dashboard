8bf0bc8e443d93b4c194b3826a3aeeed4cf1f1f4
Javier <chavi.sennin@gmail.com>
2021-02-03 15:01:00 -0600

    Se agrego nuevamente el menu en vertical para la version de tablet

--- js/efects.js
+++ js/efects.js
@@ -1,2 +1,31 @@
-const screenTablet = matchMedia("screen and (max-width: 767px)") //Agregamos nediaquerry
-const manu = document
+const tabletScreen = matchMedia("screen and (max-width: 767px)") //Agregamos nediaquerry
+const Menu = document.querySelector('.menu')
+console.log(Menu)
+const btnBurger = document.querySelector('#btn-burger')
+console.log(btnBurger)
+
+tabletScreen.addListener(validationTablet)
+
+function validationTablet(changeTablet) {
+    if( changeTablet.matches ) {
+        console.log('Estoy cambiando a modo tableta')
+        
+        btnBurger = document.addEventListener('click', muestraoculta)
+    }
+    else {
+        console.log('Pase a otra pantalla')
+        
+        btnBurger = document.addEventListener('click', muestraoculta)
+    }
+}
+
+validationTablet(tabletScreen)
+
+function muestraoculta() {
+    if(Menu.contains('activo')) {
+        Menu.classList.remove('activo')
+    }
+    else {
+        Menu.classList.add('activo')
+    }
+}
