8848d4232eebedbec4208d1a4aa7327b0f9f0a10
Javier <chavi.sennin@gmail.com>
2021-03-01 18:33:55 -0600

    El menu funciona correctamente. Se agregaron todos los checkbox de la pestaña vehiculos, se intenta agregar un estilo a los checkbox

--- js/templates.js
+++ js/templates.js
@@ -5,13 +5,28 @@ new Vue ({
         return {
             title: "Que onda",
 
-            show: false
+            showValue: 0
         }
     },
 
     methods: {
-        showForm() {
-            this.show = !this.show
+        showForm1() {
+            this.showValue = 1 
+        },
+        showForm2() {
+            this.showValue = 2 
+        },
+        showForm3() {
+            this.showValue = 3 
+        },
+        showForm4() {
+            this.showValue = 4 
+        },
+        showForm5() {
+            this.showValue = 5 
+        },
+        showForm6() {
+            this.showValue = 6 
         }
     }
 })
